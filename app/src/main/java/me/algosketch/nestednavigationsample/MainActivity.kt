2c78545858a3b18d366c4f7281dc577cf9a3d18e
algosketch <algosketch@gmail.com>
2024-04-25 08:05:58 +0900

    nested navigation 예시 구현

--- app/src/main/java/me/algosketch/nestednavigationsample/MainActivity.kt
+++ app/src/main/java/me/algosketch/nestednavigationsample/MainActivity.kt
@@ -3,13 +3,24 @@ package me.algosketch.nestednavigationsample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import me.algosketch.nestednavigationsample.ui.theme.NestedNavigationSampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +28,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NestedNavigationSampleTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainNavHost()
                 }
             }
         }
@@ -30,17 +40,72 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainNavHost() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable(route = "home") {
+            HomeScreen(
+                navigateToDetail = { navController.navigate("detail/$it") }
+            )
+        }
+
+        navigation(
+            route = "detail/{id}",
+            startDestination = "summary",
+            arguments = listOf(
+                navArgument("id") { type = NavType.IntType }
+            )
+        ) {
+            composable(route = "summary") { entry ->
+                val id = entry.arguments!!.getInt("id")
+                DetailScreen(
+                    id = id,
+                    navigateToDescription = { navController.navigate("description") }
+                )
+            }
+
+            composable(route = "description") { entry ->
+                val id = entry.arguments!!.getInt("id")
+                DetailContent(id)
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(
+    navigateToDetail: (Int) -> Unit,
+) {
+    val contents = (1..100).toList()
+
+    LazyColumn {
+        items(contents) { id ->
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(40.dp)
+                    .clickable { navigateToDetail(id) },
+                text = "상세 보기 $id",
+            )
+        }
+    }
+}
+
+@Composable
+fun DetailScreen(
+    id: Int,
+    navigateToDescription: () -> Unit,
+) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        modifier = Modifier
+            .fillMaxSize()
+            .clickable(onClick = navigateToDescription),
+        text = "id ${id}에 대한 상세 : 상세 화면에서는 이렇게 내용이 적혀 있는데, 만약 내용이 길어지면 내용 클릭을 통해 ...더보기",
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    NestedNavigationSampleTheme {
-        Greeting("Android")
-    }
+fun DetailContent(id: Int) {
+    Text("id ${id}에 대한 상세 : 상세 화면에서는 이렇게 내용이 적혀 있는데, 만약 내용이 길어지면 내용 클릭을 통해 전체 내용을 확인할 수 있다.")
 }
