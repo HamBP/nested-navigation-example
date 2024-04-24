3caecea1d28c16a405261919c5782d5e2515a634
algosketch <algosketch@gmail.com>
2024-04-25 08:46:04 +0900

    feat : ViewModel를 통해 문제 해결

--- app/src/main/java/me/algosketch/nestednavigationsample/MainActivity.kt
+++ app/src/main/java/me/algosketch/nestednavigationsample/MainActivity.kt
@@ -2,6 +2,7 @@ package me.algosketch.nestednavigationsample
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,8 +14,10 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -64,7 +67,8 @@ fun MainNavHost() {
                 val id = entry.arguments!!.getInt("id")
                 DetailScreen(
                     id = id,
-                    navigateToDescription = { navController.navigate("description") }
+                    navigateToDescription = { navController.navigate("description") },
+                    popBackstack = navController::popBackStack,
                 )
             }
 
@@ -99,11 +103,32 @@ fun HomeScreen(
 fun DetailScreen(
     id: Int,
     navigateToDescription: () -> Unit,
+    popBackstack: () -> Unit,
+    viewModel: DetailViewModel = viewModel(),
 ) {
+    LaunchedEffect(Unit) {
+        viewModel.events.collect { event ->
+            when (event) {
+                DetailEvent.NavigateUp -> {
+                    viewModel.preventEvents()
+                    popBackstack()
+                }
+
+                DetailEvent.NavigateToDescription -> navigateToDescription()
+            }
+        }
+    }
+
+    BackHandler {
+        // 여기서 prevent를 호출할 수도 있지만, 일관된 처리를 위해 이벤트를 경유한다.
+        // 실제로는 시스템 백버튼 뿐만 아니라 뒤로가기 버튼도 처리할 가능성이 높다.
+        viewModel.sendEvent(DetailEvent.NavigateUp)
+    }
+
     Text(
         modifier = Modifier
             .fillMaxSize()
-            .clickable(onClick = navigateToDescription),
+            .clickable { viewModel.sendEvent(DetailEvent.NavigateToDescription) },
         text = "id ${id}에 대한 상세 : 상세 화면에서는 이렇게 내용이 적혀 있는데, 만약 내용이 길어지면 내용 클릭을 통해 ...더보기",
     )
 }
